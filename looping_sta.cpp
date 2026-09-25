2d20f6e0c71d8f033c9963f4d1ee18dccedd015d
Yashuugowda <yashodhay234@gmail.com>
2026-09-25 21:14:34 +0530

    day3

---
+++ looping_sta.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+/*
+Task: Number Analysis using while loop
+
+- Read numbers until user enters 0.
+- No array or for loop.
+- Handle the case when the first input is 0.
+*/
+int main(){
+    
+    int num;
+    while(num!=0){
+        cout<<"enter the num:";
+        cin>>num;
+    }
+}
